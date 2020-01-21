438f2ea3237e93ed65d1dffc9b946984bcd815f5
Tyler Kotnour <tlkotnour@gmail.com>
2020-01-20 21:27:40 -0600

    wrote pipelines & settings

--- canna/canna/pipelines.py
+++ canna/canna/pipelines.py
@@ -5,7 +5,22 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+from scrapy.exporters import CsvItemExporter
 
-class CannaPipeline(object):
+class WriteItemPipeline(object):
+
+    def __init__(self):
+        self.filename = 'strain_data.csv'
+
+    def open_spider(self, spider):
+        self.csvfile = open(self.filename, 'wb')
+        self.exporter = CsvItemExporter(self.csvfile)
+        self.exporter.start_exporting()
+
+    def close_spider(self, spider):
+        self.exporter.finish_exporting()
+        self.csvfile.close()
+
     def process_item(self, item, spider):
+        self.exporter.export_item(item)
         return item
